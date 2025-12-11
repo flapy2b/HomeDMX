cc77c4dfb47d9f8c35393fa4cf8f9baaab005468
GUENIOT FLORIAN <gueniot_f@univ-corse.fr>
2025-12-11 21:30:35 +0100

    Update Menu Bar

---
+++ HomeDMX/LiquidGlassTabBar.swift
@@ -0,0 +1,18 @@
+//
+//  LiquidGlassTabBar.swift
+//  HomeDMX
+//
+//  Created by GUENIOT FLORIAN on 11/12/2025.
+//
+
+import SwiftUI
+
+struct LiquidGlassTabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LiquidGlassTabBar()
+}
