24e0f39ac4613c1fec9a1af3562170c73c7b6422
GUENIOT FLORIAN <gueniot_f@univ-corse.fr>
2025-12-11 15:18:47 +0100

    Initial Commit

---
+++ HomeDMX/HomeDMXApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeDMXApp.swift
+//  HomeDMX
+//
+//  Created by GUENIOT FLORIAN on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct HomeDMXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
