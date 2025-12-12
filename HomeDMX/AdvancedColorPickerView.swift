c1dcecbffc930aa13731172ca82bbe61c9fc7231
GUENIOT FLORIAN <gueniot_f@univ-corse.fr>
2025-12-12 10:43:02 +0100

    Ajout d'un colorwheel

---
+++ HomeDMX/AdvancedColorPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  AdvancedColorPickerView.swift
+//  HomeDMX
+//
+//  Created by GUENIOT FLORIAN on 11/12/2025.
+//
+
+import SwiftUI
+
+struct AdvancedColorPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdvancedColorPickerView()
+}
---
+++ HomeDMX/ColorWheelView.swift
@@ -0,0 +1,18 @@
+//
+//  ColorWheelView.swift
+//  HomeDMX
+//
+//  Created by GUENIOT FLORIAN on 12/12/2025.
+//
+
+import SwiftUI
+
+struct ColorWheelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorWheelView()
+}
