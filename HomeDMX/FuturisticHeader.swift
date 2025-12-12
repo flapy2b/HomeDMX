9ad89e1b3b42fa7562d3cd41865776acfdec7781
GUENIOT FLORIAN <gueniot_f@univ-corse.fr>
2025-12-12 11:41:41 +0100

    Ajout du titre

---
+++ HomeDMX/FuturisticHeader.swift
@@ -0,0 +1,18 @@
+//
+//  FuturisticHeader.swift
+//  HomeDMX
+//
+//  Created by GUENIOT FLORIAN on 12/12/2025.
+//
+
+import SwiftUI
+
+struct FuturisticHeader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FuturisticHeader()
+}
